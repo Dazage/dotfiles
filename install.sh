76663fd1d04e4a13dd7911acd7b95486ec267368
dazage <git@dmt.is>
2023-04-28 18:08:59 +0000

    automate install + gitignore *-install.log

--- install.sh
+++ install.sh
@@ -1,16 +1,73 @@
 #!/usr/bin/env bash
-mkdir -pv $HOME/.config/tmux/plugins/
-git clone https://github.com/tmux-plugins/tpm $HOME/.config/tmux/plugins/tpm
-stow -t $HOME.config/tmux/ tmux && echo 'congrats, tmux is installed!'
-## TODO make tmux auto-install plugins as part of this script
 
-# NvChad setup
-# TODO add NvChad pre-requisites
-# - ripgrep
-# - npm
-# - nerd font for ligatures
-## set up NvChad
-[[ ! -d $HOME/.config/neovim/ ]] \
-  && git clone "https://github.com/NvChad/NvChad" "$HOME/.config/nvim" --depth 1 \
-  || echo 'Error! $HOME/.config/nvim/ already exists!'; exit 1
-## TODO add my custom config
+pkginstall() {
+  $pkginst "$@" >> "$HOME/.dotfiles/$(date +%F)-install.log"
+}
+
+setup_fonts() {
+  stow -t "$HOME" fonts
+}
+
+setup_git() {
+  stow -t "$HOME" git
+}
+
+setup_gnupg() {
+  stow -t "$HOME" gnupg
+}
+
+# TODO add my custom nvim config
+# TODO auto-add nerd font for ligatures
+setup_nvim() {
+  # install neovim and pre-requisites
+  pkginstall neovim ripgrep npm
+  # set up NvChad
+  if [[ ! -d $HOME/.dotfiles/nvim/.config/nvim/ ]]; then 
+    git clone "https://github.com/NvChad/NvChad" "$HOME/.dotfiles/nvim/.config/nvim/" --depth 1
+    # apply custom config
+    #rm -r $HOME/.dotfiles/nvim/.config/nvim/lua/custom/
+    #cp -r $HOME/.dotfiles/nvchad-custom/custom/ $HOME/.dotfiles/nvim/.config/nvim/lua/custom/
+    echo 'congrats, make sure to run neovim to finish setup.'
+  fi
+}
+
+setup_scripts() {
+  echo 'uh oh stinky'
+}
+
+# My shell consists of Zsh and some scripts.
+setup_shell() {
+  pkginstall zsh
+  stow -t "$HOME" shell
+}
+
+# TODO make tmux auto-install plugins as part of this script
+setup_tmux() {
+  git clone https://github.com/tmux-plugins/tpm $HOME/.dotfiles/tmux/.config/tmux/plugins/tpm > /dev/null 2>&1
+  stow -t "$HOME" tmux
+  pkginstall tmux 
+}
+
+setup_placeholders() {
+  stow -t "$HOME" z-placeholders
+}
+
+# DETERMINE PACKAGE MANAGER
+which brew > /dev/null 2>&1 && pkginst="brew install -y"
+which dnf > /dev/null 2>&1 && pkginst="sudo dnf install -y"
+which pacman > /dev/null 2>&1 && pkginst="sudo pacman -S -y"
+which pkg > /dev/null 2>&1 && pkginst="pkg install -y"
+
+# Warning for linux users
+[[ $OSTYPE -eq 'linux-gnu' ]] && echo 'WARNING: You may need to input your super user password to install packages.'
+
+# PROGRAM STARTS
+setup_fonts
+setup_git
+setup_gnupg
+#setup_scripts
+setup_shell
+setup_tmux
+setup_nvim
+setup_placeholders
+
